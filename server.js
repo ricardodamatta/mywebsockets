259a81e6808a5cfe8151ad087796cc11fb550dfc
Ricardo DaMatta <ricardo.damatta@gmail.com>
2013-06-13 12:09:42 -0400

    2nd commit: sever.js comment for process.env.IP

--- server.js
+++ server.js
@@ -1,26 +1,30 @@
-var app = require('http').createServer(handler)
-, io = require('socket.io').listen(app)
-, fs = require('fs')
+var app = require('http').createServer(handler),
+    io = require('socket.io').listen(app),
+    fs = require('fs')
+// had to add ", process.env.IP || '127.0.0.1'" below to make the process work
+    app.listen(process.env.PORT || 8001, process.env.IP || '127.0.0.1');
 
-app.listen(process.env.PORT || 8001, process.env.IP || '127.0.0.1');
+function handler(req, res) {
+    fs.readFile('index.html',
 
-function handler (req, res) {
-fs.readFile('index.html',
-function (err, data) {
-if (err) {
-res.writeHead(500);
-return res.end('Error loading index.html');
-}
+    function (err, data) {
+        if (err) {
+            res.writeHead(500);
+            return res.end('Error loading index.html');
+        }
 
-res.writeHead(200, {'Content-Type': 'text/html', "Content-Length": data.length});
-res.end(data);
-});
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+            "Content-Length": data.length
+        });
+        res.end(data);
+    });
 }
 
 io.sockets.on('connection', function (socket) {
-// echo the message
-socket.on('message', function (data) {
-console.info(data);
-socket.send("[ECHO] "+data);
-});
+    // echo the message
+    socket.on('message', function (data) {
+        console.info(data);
+        socket.send("[ECHO] " + data);
+    });
 });
